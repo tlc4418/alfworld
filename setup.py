fa563a58f4c2a6e123bdc4fcccd51216f4057e85
Yicheng Luo <ethanluoyc@gmail.com>
2023-10-09 20:37:07 +0100

    Relax dependency

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(
     license=open('LICENSE').read(),
     zip_safe=False,
     description="ALFWorld - Aligning Text and Embodied Environments for Interactive Learning.",
-    install_requires=[line for line in open('requirements.txt').readlines() if "@" not in line],
+    install_requires=[line for line in open('requirements.txt').readlines() if "@" not in line and not line.startswith("#")],
 )
